15a3b1ed7b4b3152fdb3809a0b6ae4aad6e176c7
prince1111 <anualli@172-27-222-197.dynapool.nyu.edu>
2015-04-09 18:22:49 -0400

    calculator

--- salesBarchart/src/salesbarchart/SalesBarchart.java
+++ salesBarchart/src/salesbarchart/SalesBarchart.java
@@ -1,3 +1,4 @@
+
  //Write a program that asks the user to enter today’s sales for five stores. 
 //The program should display a bar chart comparing each store’s sales. 
 //Create each bar in the bar chart by displaying a row of asterisks. 
@@ -8,7 +9,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author Anu Alli
+ * @author Anu Allilli
  */
 public class SalesBarchart {
 
@@ -17,9 +18,9 @@ public class SalesBarchart {
         store = new double[5];
 
         try (Scanner userInput = new Scanner(System.in)) {
-            for (int i = 0; i < 5; i++) {
-                System.out.printf(" please enter today\'s sales from store %d: ", i + 1);
-                store[i] = userInput.nextDouble();
+            for (int intial = 0; intial < 5; intial++) {
+                System.out.printf(" please enter today\'s sales for store %d: ", intial + 1);
+                store[intial] = userInput.nextDouble();
             }
 
             System.out.println("\nSALES BAR CHART");
@@ -35,7 +36,7 @@ public class SalesBarchart {
             }
 
             userInput.close();
-
+            
         }
 
     }
